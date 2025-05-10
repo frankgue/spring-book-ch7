123de8ef4c197e538da7114f4bad7227154c617f
frankguekeng <frankguekeng11@gmail.com>
2025-05-10 17:02:14 +0100

    Inheriting Context Configuration

---
+++ context_configuration_and_caching/src/test/java/com/wiley/beginningspring/ch7/ChildTest.java
@@ -0,0 +1,19 @@
+package com.wiley.beginningspring.ch7;
+
+import com.wiley.beginningspring.ch7.model.Bar;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration("classpath:/subContext.xml")
+public class ChildTest extends BaseTest{
+    @Autowired
+    private Bar bar;
+
+    @Test
+    public void dependenciesShouldBeAvailable(){
+        Assert.assertNotNull(foo);
+        Assert.assertNotNull(bar);
+    }
+}
